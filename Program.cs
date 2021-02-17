ea250bf8bc3b161bd247edf7d382cf5e3cdfc026
renaudesign <renaud.sourmail@gmail.com>
2021-02-17 14:37:14 +0100

    Boucles 1 exercice terminé

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSHARP_Boucles_Exo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            while (count < 10) 
+            {
+            Console.WriteLine("Ceci n'est qu'un message de test. Veuillez ne pas y prêter attention");
+            count++;
+            }
+        }
+    }
+}
